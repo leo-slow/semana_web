d129dfd5d5d16c3dc0a48df54411938a207129e6
sammy-ulfh <sammy_ulfh@proton.me>
2025-10-30 01:15:21 -0600

    Simple page

---
+++ DignidadHumana/src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card({ name, years, originPlace, description }){
+  return(
+    <div className="w-[28%] h-auto p-[1%] m-[2%] content-center justify-center rounded-2xl border-dashed border-3 bg-black hover:bg-gray-500">
+      <h2>{name}</h2>
+      <h3>{years}</h3>
+      <h4>{originPlace}</h4>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default Card;
